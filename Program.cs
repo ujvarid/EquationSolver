e66fe02de101384284b72bbc9199cd939baada1e
ujvarid <156822242+ujvarid@users.noreply.github.com>
2024-06-27 20:59:48 +0200

    Added Bolzano

--- Program.cs
+++ Program.cs
@@ -1,65 +1,55 @@
-﻿namespace EquationSolver
+﻿using System.Linq.Expressions;
+
+namespace EquationSolver
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // for testing
-
-            /*Console.Write("Adj meg egy függvényt: f(x) = ");
-            string line = Console.ReadLine();
-            Console.Write("Milyen értéket akarsz behelyettesíteni: ");
-            double value = Convert.ToDouble(Console.ReadLine());
-            string [] exp = line.Split(' ');
-            List<string> expression = new List<string>();
-            for(int i = 0; i < exp.Length; i++) 
-            {
-                expression.Add(exp[i]);
-            }
-            expression = PolishNotation.ToPolishForm(expression, false);
-            expression = PolishNotation.ChangeX(expression, value);
-            Console.WriteLine();
-            Console.WriteLine(PolishNotation.EvaluatePolishForm(expression));*/
-
-            // brute force solving
-
 
             //input
 
             Console.WriteLine("Adj meg egy 0 - ra rendezett egyenletet:\n");
+
             string line = Console.ReadLine();
             System.Console.Clear();
             Console.Write("Az egyenlet: \n\n" + line + " = 0");
             string[] exp = line.Split(' ');
             List<string> expression;
-            List<string> expressionConst = new List<string>();
+            List<string> expressionConst = new();
 
             for (int i = 0; i < exp.Length; i++)
             {
                 expressionConst.Add(exp[i]);
             }
 
-            // brute force solution, not using Bolzano's theorem
+            // using Bolzano's Intermediate Value Theorem
 
-            double x = -69;
+            CopyLists(expressionConst, out expression);
+            int a, b;
 
-            for (int i = -999999; i < 999999; i++)
+            // 1. step: finding two places where the signs of the function values ​​differ
+
+            (a, b) = PlacesWhereTheSignsDiffer(expression);
+
+            // 2. step: interval halving
+
+            (a, b) = IntervalHalving(expression, a, b);
+
+            if (a == b)
             {
-                CopyLists(expressionConst, out expression);
-                expression = PolishNotation.ToPolishForm(expression, false);
-                expression = PolishNotation.ChangeX(expression, i);
-                if (PolishNotation.EvaluatePolishForm(expression) == 0)
-                {
-                    x = i;
-                    break;
-                }
+                Console.WriteLine();
+                Console.WriteLine("\n\nA megoldás: \n\nx = " + a);
+                Console.ReadKey(); 
             }
-
-
-            Console.WriteLine();
-            Console.WriteLine("\n\nA megoldás: \n\nx = " + x);
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n\nA megoldás: \n\nx: " + a + " és " + b + " között van");
+                Console.ReadKey(); 
+            }
         }
-    
+
         public static void CopyLists(List<string> list, out List<string> list2) 
         {
             list2 = new List<string>();
@@ -67,6 +57,67 @@
             {
                 list2.Add(list[i]);
             }
+        }
+
+        public static (int a, int b) PlacesWhereTheSignsDiffer(List<string> function)
+        {
+            int a = FindSignChangePoint(function, positive: false);
+            int b = FindSignChangePoint(function, positive: true);
+            return (a, b);
+        }
+
+        public static int FindSignChangePoint(List<string> function, bool positive)
+        {
+            int k = 0;
+            List<string> expressionConst = new List<string>(function);
+
+            for (int i = positive ? 99999 : -99999; positive ? i >= -999 : i <= 999; i += positive ? -1 : 1)
+            {
+                CopyLists(expressionConst, out function);
+                function = PolishNotation.ToPolishForm(function, false);
+                function = PolishNotation.ChangeX(function, i);
+                double evalResult = PolishNotation.EvaluatePolishForm(function);
+                if ((positive && evalResult > 0) || (!positive && evalResult < 0))
+                {
+                    k = i;
+                    break;
+                }
+            }
+            return k;
+        }
+
+        public static (int, int) IntervalHalving(List<string> function, int a, int b)
+        {
+            List<string> expressionConst = new();
+
+            for (int i = 0; i < function.Count; i++)
+            {
+                expressionConst.Add(function[i]);
+            }
+
+            int z = (a + b) / 2;
+
+            for(int i = 0; i < 5000; ++i)
+            {
+                CopyLists(expressionConst, out function);
+                function = PolishNotation.ToPolishForm(function, false);
+                function = PolishNotation.ChangeX(function, z);
+                double evalResult = PolishNotation.EvaluatePolishForm(function);
+                if (evalResult == 0)
+                {
+                    return (z, z);
+                }
+                else if (evalResult > 0)
+                {
+                    b = z;
+                }
+                else
+                {
+                    a = z;
+                }
+                z = (a + b) / 2;
+            }
+            return (a, b);
         }
     }
 }
